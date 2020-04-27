1092d3323263b523dae3625fb971c5b3e20d5fa5
ddock4you <ddock4you@gmail.com>
2020-04-27 16:50:43 +0900

    - 리덕스 표현 최고 단축 형태(?)

--- redux-practice/src/containers/CounterContainer.js
+++ redux-practice/src/containers/CounterContainer.js
@@ -9,31 +9,9 @@ const CounterContainer = ({ number, increase, decrease }) => {
     );
 };
 
-// mapStateToProps
-// 리덕스 스토어 안의 상태를 컴포넌트의 props로 넘겨주기 위해 설정하는 함수
-
-// const mapStateToProps = (state) => ({
-//     number: state.counter.number,
-// });
-
-// // 액션 생성 함수를 컴포넌트의 props로 넘겨주기 위해 사용하는 함수
-// const mapDispatchToProps = (dispatch) => ({
-//     increase: () => {
-//         dispatch(increase());
-//     },
-//     decrease: () => {
-//         dispatch(decrease());
-//     },
-// });
-
-// export default connect(mapStateToProps, mapDispatchToProps)(CounterContainer);
-
 export default connect(
     (state) => ({
         number: state.counter.number,
     }),
-    (dispatch) => ({
-        increase: () => dispatch(increase()),
-        decrease: () => dispatch(decrease()),
-    })
+    { increase, decrease }
 )(CounterContainer);
