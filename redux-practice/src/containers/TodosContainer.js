375aeb075810555a667adda878014e0acb4fcc95
ddock4you <ddock4you@gmail.com>
2020-04-28 15:28:26 +0900

    - useAction 사용하여 todoList에 적용

--- redux-practice/src/containers/TodosContainer.js
+++ redux-practice/src/containers/TodosContainer.js
@@ -1,21 +1,18 @@
 import React, { useCallback } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import Todos from "../components/Todos";
 import { changeInput, insert, toggle, remove } from "../modules/todos";
+import Todos from "../components/Todos";
+import useAction from "../lib/useAction";
 
 const TodosContainer = () => {
     const { input, todos } = useSelector(({ todos }) => ({
         input: todos.input,
         todos: todos.todos,
     }));
-    const dispatch = useDispatch();
-    const onChangeInput = useCallback((input) => dispatch(changeInput(input)), [
-        dispatch,
-    ]);
-    const onInsert = useCallback((text) => dispatch(insert(text)), [dispatch]);
-    const onToggle = useCallback((id) => dispatch(toggle(id)), [dispatch]);
-    const onRemove = useCallback((id) => dispatch(remove(id)), [dispatch]);
-
+    const [onChangeInput, onInsert, onToggle, onRemove] = useAction(
+        [changeInput, insert, toggle, remove],
+        []
+    );
     return (
         <Todos
             input={input}
@@ -49,4 +46,13 @@ const TodosContainer = () => {
 //     }
 // )(TodosContainer);
 
-export default TodosContainer;
+// connect 함수를 사용할 경우 해당 컨테이너 컴포넌트의 부모컴포넌트가 리렌더링될 때
+// 해당 컨테이너 컴포넌트의 props가 바뀌지 않았다면 리렌더링이 자동으로 방지되어
+// 성능이 최적화 됨.
+
+// useSelector를 사용하여 리덕스 상태를 조회했을 때는 이 최적화 작업이 자동으로
+// 이루어지지 않으므로 성능 최적화를 위해서는 REact.memo를 컨테이너 컴포넌트에
+// 사용해 주어야 합니다.
+// 지금은 부모 컴포넌트인 App 컴포넌트가 리렌더링되는 일이 없으므로 불펼요한 성능 최적화임.
+
+export default React.memo(TodosContainer);
