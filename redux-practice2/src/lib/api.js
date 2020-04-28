7b1cd2cda2192a5e6aa42675883ee3b8fd974f39
ddock4you <ddock4you@gmail.com>
2020-04-29 01:17:21 +0900

    - redux-thunk 코드 최적화

--- redux-practice2/src/lib/api.js
+++ redux-practice2/src/lib/api.js
@@ -1,11 +1,9 @@
 import axios from "axios";
 
 export const getPost = (id) => {
-    axios.get(`http://jsonplaceholder.typicode.com/posts/${id}`);
-    // const test = await axios.get(`http://jsonplaceholder.typicode.com/posts/1`);
-    // console.log(test);
+    return axios.get(`http://jsonplaceholder.typicode.com/posts/${id}`);
 };
 
 export const getUsers = () => {
-    axios.get(`http://jsonplaceholder.typicode.com/users`);
+    return axios.get(`http://jsonplaceholder.typicode.com/users`);
 };
