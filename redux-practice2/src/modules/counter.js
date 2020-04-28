6426b666afbe01b1f40984c6431bc9ad1174fc4d
ddock4you <ddock4you@gmail.com>
2020-04-28 16:49:16 +0900

    - 리덕스 미들웨어 실습을 위한 redux-practice2 예제 생성 수정

--- redux-practice2/src/modules/counter.js
+++ redux-practice2/src/modules/counter.js
@@ -6,6 +6,8 @@ const DECREASE = "counter/DECREASE";
 export const increase = createAction(INCREASE);
 export const decrease = createAction(DECREASE);
 
+// 액션 객체 내 데이터는 나중에 상태 업데이트할 때 참고해야할 값들
+
 const initialState = 0;
 
 const counter = handleActions(
